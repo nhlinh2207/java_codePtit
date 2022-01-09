3a515590b378711f9c4162e5df48cfe0eaa125e3
nhlinh2207 <nguyenhoailinh2207@mail.com>
2022-01-09 08:53:32 +0700

    sua lai

--- codeptit.java
+++ codeptit.java
@@ -1,57 +1,95 @@
 package codePtit;
-import java.util.Scanner;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
 
 public class Test {
     static class sv{
-        private String name;
-        private long luongcb;
-        private long ngaycong;
-        private String chucvu;
-        private long thuong;
-        private long phucap;
+        private String ma;
+        private String ten;
+        private int tinchi;
+        private String lith;
+        private String thchanh;
 
         public sv(){}
 
-        public void nhap(Scanner s){
-            name = s.nextLine();
-            luongcb = s.nextLong();
-            ngaycong = s.nextLong();
-            chucvu = s.next();
+        public void setMa(String ma){
+            this.ma = ma;
         }
 
+        public String getMa(){return ma;}
 
-        public void setThuong(){
-            if (ngaycong >= 25)
-                thuong = Math.round(20.0/100*ngaycong*luongcb);
-            else if (ngaycong >= 22 && ngaycong < 25)
-                thuong = Math.round(10.0/100*ngaycong*luongcb);
-            else thuong = 0;
+        public String getTen() {
+            return ten;
         }
 
-        public void setPhucap(){
-            if (chucvu.equals("GD"))
-                phucap = 250000;
-            else if (chucvu.equals("PGD"))
-                phucap = 200000;
-            else if (chucvu.equals("TP"))
-                phucap = 180000;
-            else phucap = 150000;
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public int getTinchi() {
+            return tinchi;
+        }
+
+        public void setTinchi(int tinchi) {
+            this.tinchi = tinchi;
         }
 
         public void xuat(){
-            System.out.println(
-                    "NV01 "+name+" "+(ngaycong*luongcb)+" "+thuong+" "+phucap+" "+(ngaycong*luongcb+thuong+phucap)
-            );
+            System.out.println(ma+" "+ten+" "+tinchi+" "+lith+" "+thchanh);
+        }
+
+        public String getLith() {
+            return lith;
+        }
+
+        public void setLith(String lith) {
+            this.lith = lith;
+        }
+
+        public String getThchanh() {
+            return thchanh;
+        }
+
+        public void setThchanh(String thchanh) {
+            this.thchanh = thchanh;
         }
     }
-    public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        sv a = new sv();
-        a.nhap(s);
-        a.setThuong();
-        a.setPhucap();
-        a.xuat();
+    public static void main(String[] args) throws IOException {
+        Scanner s = new Scanner(new File("MONHOC.in"));
+        int c = 1;
+        sv a = null;
+        List<sv> res = new ArrayList<>();
+        Integer t = Integer.valueOf(s.nextLine());
+        while (s.hasNextLine()){
+            if (c ==1) {
+                a = new sv();
+                a.setMa(s.nextLine());
+                c++;
+            }else if (c == 2){
+                a.setTen(s.nextLine());
+                c++;
+            }else if (c == 3){
+                a.setTinchi(Integer.valueOf(s.nextLine()));
+                c++;
+            }
+            else if (c == 4){
+                a.setLith(s.nextLine());
+                c++;
+            }
+            else if (c == 5){
+                a.setThchanh(s.nextLine());
+                c=1;
+                res.add(a);
+            }
+        }
+        Collections.sort(res,Comparator.comparing(sv::getMa));
+        for (sv i : res)
+        {
+            if (!i.getThchanh().equals("Truc tiep"))
+                i.xuat();
+        }
     }
-
 }
-
